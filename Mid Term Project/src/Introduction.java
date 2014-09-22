e1417b46f0959dbec61b66ea2ac798fc536fb772
anthony.yarosh <anthony.yarosh@MU-C3-05.mullen.pvt.k12.co.us>
2014-09-22 09:09:04 -0600

    2nd Commit

---
+++ Mid Term Project/src/Introduction.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Introduction
+	{
+	static String name;
+	
+	public static void main(String[] args)
+		{
+		Introduction1();
+		}
+
+	static void Introduction1()
+		{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome Stranger, Please Enter Your Name");
+		name = input.nextLine();
+		System.out.println("Welcome to Sword Art Online " + name);
+		System.out.println("System check: GOOD");
+		System.out.println("System Initialize");
+
+		}
+
+	}
--- Mid Term Project/src/TextAdventureSwordArtOnline.java
+++ Mid Term Project/src/TextAdventureSwordArtOnline.java
@@ -11,16 +11,65 @@ public class TextAdventureSwordArtOnline
 
 	private static void Introduction()
 		{
-		Scanner input = new Scanner(System.in);
-		System.out.println("Welcome Stranger, Please Enter Your Name");
-		String name = input.nextLine();
-		System.out.println("Welcome to Sword Art Online " + name);
-		System.out.println("System check: GOOD");
-		System.out.println("System Initialize");
+		Introduction.Introduction1();
 
 		}
+
 	private static void Aincrad()
 		{
-		
+		boolean again = true;
+		do
+			{
+
+			int option;
+
+			Scanner input = new Scanner(System.in);
+			System.out
+					.println("Welcome to Aincrad "
+							+ Introduction.name
+							+ ". "
+							+ " Even though this world is text generated, it is very much alive. Now I have a qestion for you.");
+			System.out.println("");
+			System.out.println("Do you: ");
+			System.out.println("");
+			System.out.println("<1> Plan to continue?");
+			System.out.println("<2> Want to get out of this game?");
+			option = input.nextInt();
+
+			if (option == 1)
+				{
+
+				System.out
+						.println(Introduction.name
+								+ ", you are auto teleported to the main city on floor one, The Town of Begginings. There is a new player tutorial on the left and shops on the right.");
+				System.out.println("");
+				System.out.println("Would you like to go left or right?");
+				System.out.println("");
+				System.out.println("<1> Left");
+				System.out.println("<2> Right");
+				option = input.nextInt();
+
+				if (option == 1)
+					{
+					System.out.println("You walk over to the player tutorial arena. "\"Welcome to the new player tutorial. This game was designed in Java by a North American company that barely monitors the use of the game, so any questions you might have rarely get answered, however this tutorial should provide the needed help.\"");
+					}
+				}
+			if (option == 2)
+				{
+				System.out.println("Goodbye.");
+				System.exit(0);
+				}
+
+			if (option == 1)
+				{
+				again = true;
+				}
+
+			if (option == 2)
+				{
+				again = false;
+				}
+
+			} while (again == true);
 		}
 	}
