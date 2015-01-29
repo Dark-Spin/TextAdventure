74ca3821e28aff78850a043f417b439fb1da613a
Yaroshan <yaroshan@mullennmail.com>
2015-01-28 22:37:04 -0700

    Complete remodel, trying for a new type of RPG, looking to add story and battle system

---
+++ Mid Term Project/src/Rooms.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+
+class Rooms {
+
+    public static void build(Room[][] room, final int WIDTH, final int HEIGHT) {
+
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                room[i][j] = new Room(i, "", "", null);
+            }
+        }
+
+        room[0][0].setNumber(1);
+        room[0][0].setName("Town Square");
+        room[0][0].setDescription("You are warped to Town Square on Floor 1. Please input up,down,left,right to move and get to aquire items as you see them.");
+
+        room[0][1].setNumber(2);
+        room[0][1].setName("Range");
+        room[0][1].setDescription("You move to the newbie training arena.");
+        room[0][1].setItems("sword");
+        room[0][1].setItems("shield");
+
+        room[1][0].setNumber(3);
+        room[1][0].setName("Shops");
+        room[1][0].setDescription("You are browsing town wares");
+        room[1][0].setItems("potions");
+
+        room[1][1].setNumber(4);
+        room[1][1].setName("Random Encounter");
+        room[1][1].setDescription("You meet a a guide NPC. He is the guide NPC for new players.");
+        room[1][1].setItems("Newbie Guide book");
+        
+        room[2][1].setNumber(5);
+        room[2][1].setName("Field");
+        room[2][1].setDescription("You are at the outskirts of town");
+
+    }
+
+    public static void print(Room[][] room, int x, int y) {
+
+        System.out.println(room[x][y].getDescription());
+        System.out.println("You see: " + room[x][y].getItems());
+        System.out.println();
+    }
+
+    public static void removeItem(Room[][] room, int x, int y, String item) {
+    	
+    	room[x][y].deleteItem(item);
+    }
+}
+
+class Room {
+
+    private int number;
+    private String name;
+    private String description;
+    public ArrayList<String> items = new ArrayList<>();
+
+    public Room(int number, String name, String description,
+            ArrayList<String> items) {
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setItems(String item) {
+        this.items.add(item);
+    }
+
+    public void deleteItem(String item) {
+        this.items.remove(item);
+    }
+
+    public ArrayList<String> getItems() {
+        return this.items;
+    }
+}
